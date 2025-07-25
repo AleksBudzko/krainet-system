852b885a0ca0ef8424d8c79616464715c64aabcd
Aleks Budzko <alexrfictcb@gmail.com>
2025-07-25 18:52:05 +0300

    Add Flyway migrations

---
+++ auth-service/src/main/resources/db/migration/Insert_test_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, email, first_name, last_name, role) VALUES
+('admin', 'adminPassword', 'admin@example.com', 'Admin', 'User', 'ROLE_ADMIN'),
+('user',  'userPassword',  'user@example.com',  'Normal', 'User', 'ROLE_USER');
