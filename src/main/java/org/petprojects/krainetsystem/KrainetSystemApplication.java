efe45ef5bcd4ff87907c704530a30c7a6e114646
Aleks Budzko <alexrfictcb@gmail.com>
2025-07-25 18:14:56 +0300

    Start structure

---
+++ src/main/java/org/petprojects/krainetsystem/KrainetSystemApplication.java
@@ -0,0 +1,13 @@
+package org.petprojects.krainetsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrainetSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrainetSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/petprojects/krainetsystem/KrainetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.petprojects.krainetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrainetSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
